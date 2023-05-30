6e91a0f97340cb4cbd7ac1fe7131ce9ce8b8b5bc
Lens <LensPlaysGames@gmail.com>
2023-05-29 20:41:41 -0700

    Automatically generate RSS feed, among other things

--- publish.el
+++ publish.el
@@ -1,4 +1,4 @@
-;;; publish.el --- Set up org-publish variables for Lensor Radii site -*- lexical-binding: t -*-
+;;; publish.el --- Set up org-publish variables for Lens_r blog website -*- lexical-binding: t -*-
 
 ;; Author: Rylan Lens Kellogg
 ;; Maintainer: Rylan Lens Kellogg
@@ -20,14 +20,24 @@
 
 ;;; Code:
 
-(defun file-string (file)
+;; Load minimum amount of packages.
+(require 'package)
+;; Fontify source code blocks with htmlize.
+(when (package-installed-p 'htmlize)
+  (let ((package-load-list '((htmlize t))))
+    (package-initialize)))
+
+;; Tangling source blocks from literate js and css files requires org-tangle.
+(require 'ob-tangle)
+
+(defun blub-file-string (file)
   "Return a string containing the contents of FILE."
   (interactive "fFile: ")
   (with-temp-buffer
     (insert-file-contents file)
     (buffer-string)))
 
-(defun directory-files-nodot (directory &optional full nosort count)
+(defun blub-directory-files-nodot (directory &optional full nosort count)
   "Like 'directory-files' but don't return dotfiles like \".\" or \"..\".
 
 If DIRECTORY is nil, 'default-directory' is used.
@@ -42,7 +52,7 @@ FULL, NOSORT, and COUNT are passed to 'directory-files' unchanged."
         (message "%S" files)
       files)))
 
-(defun concatenate-directory-files (directory filepath &optional append)
+(defun blub-concatenate-directory-files (directory filepath &optional append)
   "Concatenate all files in DIRECTORY and save the result into FILEPATH.
 
 If APPEND is non-nil, concatenate to the file at FILEPATH."
@@ -55,15 +65,19 @@ If APPEND is non-nil, concatenate to the file at FILEPATH."
       (when (file-regular-p file) (insert-file-contents file)))
     (write-file filepath)))
 
-;; Load minimum amount of packages.
-(require 'package)
-;; Fontify source code blocks with htmlize.
-(when (package-installed-p 'htmlize)
-  (let ((package-load-list '((htmlize t))))
-    (package-initialize)))
+(defun blub-time-to-rss2-pubdate-string (passed-time)
+  "Convert PASSED-TIME object to an rss2 specification (RFC 822) date.
+Passing nil will give the current time (as with any time object)."
+  (concat
+   (substring (format-time-string "%A" passed-time) 0 3)
+   (format-time-string ", %d " passed-time)
+   (substring (format-time-string "%B" passed-time) 0 3)
+   (format-time-string " %Y %T PDT" passed-time) ;; PST may be PDT, sometimes :Þ
+   ))
 
 ;;; Create pages/links for each blog post.
-(let ((blog-links nil))
+(let ((blog-links nil)
+      (rss2-items nil))
   (with-current-buffer (find-file-noselect "org/mainblog.org")
     ;; Ensure "org/posts/" is a valid, empty directory.
     (delete-directory "posts" t)
@@ -102,7 +116,21 @@ If APPEND is non-nil, concatenate to the file at FILEPATH."
            ;; date, to the list of blog links.
            (push `(,(format "[[file:%s][%s]]" post-file-name headline)
                    ,publish-date)
-                 blog-links))))))
+                 blog-links)
+
+           ;; Push an item to the RSS feed.
+           (push `(,(concat
+                     (format "    <item>\n      <title>%s</title>\n" headline custom-id)
+                     (format "      <link>https://lensplaysgames.github.io/lensr_blog_v1/posts/%s.html</link>\n  " custom-id)
+                     ;; Date translation...
+                     (let ((publish-time (org-timestamp-to-time (org-timestamp-from-string publish-date))))
+                       (concat "    <pubDate>"
+                               (blub-time-to-rss2-pubdate-string publish-time)
+                               "</pubDate>\n"))
+                     "    </item>\n"
+                     )
+                   ,(org-timestamp-to-time (org-timestamp-from-string publish-date)))
+                 rss2-items))))))
 
   ;; Sort links by publish date
   (sort blog-links (lambda (a b)
@@ -133,6 +161,39 @@ If APPEND is non-nil, concatenate to the file at FILEPATH."
               ))
           blog-links)
     (save-buffer))
+
+  ;; Sort rss items by publish date...
+  (sort rss2-items (lambda (a b)
+                     (not (time-less-p
+                           (cadr a)
+                           (cadr b)))))
+
+
+  ;; Save RSS in an XML file.
+  (with-current-buffer (find-file-noselect "org/rss.xml")
+    (erase-buffer)
+    (insert "<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">\n"
+            "  <channel>\n"
+            "    <title>Lens_r Blog</title>\n"
+            "    <link>https://lensplaysgames.github.io/lensr_blog_v1/</link>\n"
+            "    <description>Lens_r sometimes writes things down.</description>\n"
+            "    <language>en-us</language>\n"
+            ;; pubDate of channel == when it was created.
+            "    <pubDate>Mon, 29 May 2023 20:08:15 PDT</pubDate>\n"
+            ;; lastBuildDate of channel == when it was last modified (now).
+            "    <lastBuildDate>"
+            (blub-time-to-rss2-pubdate-string nil)
+            "</lastBuildDate>\n"
+            "    <docs>https://validator.w3.org/feed/docs/rss2.html</docs>\n"
+            "    <generator>Lens_r's Pile of Elisp</generator>\n"
+            "    <atom:link href=\"https://lensplaysgames.github.io/lensr_blog_v1/rss.xml\" rel=\"self\" type=\"application/rss+xml\"/>\n")
+    (mapc (lambda (rss-item)
+            (insert (car rss-item)))
+          rss2-items)
+    (insert "  </channel>\n"
+            "</rss>\n")
+    (save-buffer))
+
   ;; return nil simply because this bit of code is freestanding, so placing
   ;; point after the last paren and doing `C-x C-e` will evaluate the whole
   ;; thing, generating `index.org` and all the blog post files in `org/posts/`.
@@ -140,7 +201,6 @@ If APPEND is non-nil, concatenate to the file at FILEPATH."
 
 ;;; Generate JavaScript and CSS with org-babel.
 
-(require 'ob-tangle)
 ;; This populates org/js and org/css subdirectories.
 (org-babel-tangle-file "org/resources.org")
 
@@ -149,9 +209,9 @@ If APPEND is non-nil, concatenate to the file at FILEPATH."
 ;; Ensure resources subdirectory existence
 (make-directory "res" t)
 ;; Consolidate separate JS files into a single one.
-(concatenate-directory-files "org/js" "res/all.js")
+(blub-concatenate-directory-files "org/js" "res/all.js")
 ;; Consolidate separate CSS files into a single one.
-(concatenate-directory-files "org/css" "res/all.css")
+(blub-concatenate-directory-files "org/css" "res/all.css")
 
 ;; https://www.npmjs.com/package/uglify-js
 ;; install with "npm i uglify-js --location=global"
@@ -189,22 +249,23 @@ If APPEND is non-nil, concatenate to the file at FILEPATH."
 ;; Regenerate files whether they are newer or not.
 (setq org-publish-use-timestamps-flag nil)
 
+;; Don't include :tags: in org headers in the exported headline.
 (setq org-export-with-tags nil)
 
 ;; all.js inlined into header (minified if available).
 (setq org-html-scripts
       (concat "<script>"
               (if (file-exists-p "res/all.min.js")
-                  (file-string "res/all.min.js")
-                (file-string "res/all.js"))
+                  (blub-file-string "res/all.min.js")
+                (blub-file-string "res/all.js"))
               "</script>"))
 
 ;; style.css inlined into header (minified if available).
 (setq org-html-head
       (concat "<style>"
               (if (file-exists-p "res/all.min.css")
-                  (file-string "res/all.min.css")
-                (file-string "res/all.css"))
+                  (blub-file-string "res/all.min.css")
+                (blub-file-string "res/all.css"))
               "</style>"))
 
 (setq org-publish-project-alist
@@ -233,7 +294,7 @@ If APPEND is non-nil, concatenate to the file at FILEPATH."
          :publishing-directory "docs"
          :recursive t
          :base-directory "org"
-         :base-extension "png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|svg\\|ico"
+         :base-extension "png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|svg\\|ico\\|xml"
          :exclude "backup\\|backup/\\|tmp\\|tmp/"
          ;; :include ("emacs-init.org")
          )
